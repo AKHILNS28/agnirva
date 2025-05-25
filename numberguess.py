cf612aafe9d00b8884986ff51002148631b44c73
AKHILNS28 <akhilns959@gmail.com>
2025-05-25 12:18:10 +0530

    The Number Guessing Game

--- numberguess.py
+++ numberguess.py
@@ -1,8 +1,17 @@
 import random
-num=random.randint(1,100)
 print("Welcome to The Number Guessing Game")
 print("Can you guess the number correctly")
-
+print("Difficulty levels")
+print("1.Easy")
+print("2.Medium")
+print("3.Hard")
+ch=int(input("Choose Difficulty"))
+if(ch==1):
+    k=random.randint(1,10)
+elif(ch==2):
+    k=random.randint(1,50)
+else:
+    num=random.randint(1,100)
 attempt=0
 while (attempt<10):
     k=int(input("Enter your guess"))
@@ -12,4 +21,4 @@ while (attempt<10):
         print("guess is too low")
     else:
         print("guess is correct")
-        break
+        break;
